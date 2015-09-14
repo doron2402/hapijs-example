6eca44bc3bdbd689523a1ab403aea26238766719
Doron Segal <doron2402@gmail.com>
2015-09-13 17:17:45 -0700

    load all routes with a scripts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,15 @@
-var routes = require('./v1');
-routes = routes.concat(require('./v2'));
+var fs = require('fs');
+var path = require('path');
+var routesPath = './src/routes';
+var routes = [];
+function getDirectories(srcpath) {
+  return fs.readdirSync(srcpath).filter(function(file) {
+    return fs.statSync(path.join(srcpath, file)).isDirectory();
+  });
+}
+
+getDirectories(routesPath).forEach(function(dir){
+	routes = routes.concat(require('./' + dir));
+});
 
 module.exports = routes;
